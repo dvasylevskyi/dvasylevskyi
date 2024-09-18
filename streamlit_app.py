48af00ef4f9d5b5c7a5a9ab17d0e2241fa246907
dvasylevskyi <dvasylevskyi@beechhallschool.org>
2024-09-18 08:56:38 +0100

    added from 1 to 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
-import streamlit as st
-import pandas as pd
-import math
+st.title("Register")
+def menu():
+  select =st.text_input("enter L to log in or R to register").lower()
+  if select == "1"
+  login()
+  
 
