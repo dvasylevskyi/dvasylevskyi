4458865ac189482608442ed91b63f26b88694513
dvasylevskyi <dvasylevskyi@beechhallschool.org>
2024-09-18 09:15:09 +0100

    e instead of E

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+
+
 st.title("Register")
 def menu():
   select =st.text_input("enter L to log in or R to register").lower()
@@ -8,10 +10,10 @@ def menu():
   else:
     menu()
 def register():
-  surmane = st.text_input("Enter your surname").lower()
-  forename = st.text_input("Enter your forename").lower()
-  birthday = st.text_input("Enter your birthday like this (DD/MM/YY)")
-  password = st.text_input("Enter your password")
+  surmane = st.text_input("enter your surname").lower()
+  forename = st.text_input("enter your forename").lower()
+  birthday = st.text_input("enter your birthday like this (DD/MM/YY)")
+  password = st.text_input("enter your password")
   username = surname[0] + surname[1] + forename[1] + birthday[2] 
   
     
