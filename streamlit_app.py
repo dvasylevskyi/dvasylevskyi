959bbbd75f9a65903908e80371a850b19c8702f5
dvasylevskyi <dvasylevskyi@beechhallschool.org>
2024-09-18 09:01:33 +0100

    added spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,12 @@ st.title("Register")
 def menu():
   select =st.text_input("enter L to log in or R to register").lower()
   if select == "1":
-  login()
-elif select == "r":
-register()
+      login()
+  elif select == "r":
+    register() 
+    
+
+
 
 
   
