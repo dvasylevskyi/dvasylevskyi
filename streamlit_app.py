d2e32cd02f56160d5e49e89538e64f5e1e7aeaaf
dvasylevskyi <dvasylevskyi@beechhallschool.org>
2024-09-19 10:02:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("Register or Login")
 
 def menu():
   select = st.text_input("enter L to log in or R to register").lower()
-  if select == "1":
+  if select == "l":
       login()
   elif select == "r":
       register()
